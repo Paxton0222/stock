3bb44a97c4344de33b29208c4cc18166dc0b6785
paxtonBravo <96046448+paxtonBravo@users.noreply.github.com>
2024-05-07 21:00:50 +0800

    update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+setup(
+    name="stock",
+    version="0.0.1",
+    description="股票查詢API",
+    author="Paxton Li",
+    author_email="paxton900222@gmail.com",
+    url="https://github.com/Paxton0222/stock.git",
+    install_requires=[],
+    packages=[],
+    package_dir={},
+    package_data={},
+    license="MIT license",
+    zip_safe=False,
+    keywords="taiwan stock api",
+    classifiers=[],
+)
